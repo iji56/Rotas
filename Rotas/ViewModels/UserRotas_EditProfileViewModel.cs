1a32ded4d7137abc0083bd74e18dbeb421b7b93a
iji56 <ijimran56@gmail.com>
2022-01-08 21:08:28 +0500

    Final

---
+++ Rotas/ViewModels/UserRotas_EditProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Rotas.ViewModels
+{
+    public class UserRotas_EditProfileViewModel
+    {
+        public UserRotas_EditProfileViewModel()
+        {
+        }
+    }
+}
